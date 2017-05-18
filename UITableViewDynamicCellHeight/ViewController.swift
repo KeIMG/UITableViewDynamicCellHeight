2439646be35187e66001550fb85686f28f9bc0b0
Ke Ma <ke.ma@img.com>
2017-05-18 09:40:20 +0100

    Add empty Xcode project

---
+++ UITableViewDynamicCellHeight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITableViewDynamicCellHeight
+//
+//  Created by Ke Ma on 18/05/2017.
+//  Copyright © 2017 Ke Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
